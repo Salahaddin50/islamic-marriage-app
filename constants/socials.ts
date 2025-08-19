5211258ab0d874f94582088e46444bfba7fcd1bf
Salahaddin50 <salahaddin665@gmail.com>
2025-08-19 17:41:14 +0300

    saferequire changed back

--- constants/socials.ts
+++ constants/socials.ts
@@ -1,12 +1,12 @@
-const facebook = safeRequire("../assets/icons/socials/facebook.png");
-const instagram = safeRequire("../assets/icons/socials/instagram.png");
-const linkedin = safeRequire("../assets/icons/socials/linkedin.png");
-const messenger = safeRequire("../assets/icons/socials/messenger.png");
-const titktok = safeRequire("../assets/icons/socials/tiktok.png");
-const twitter = safeRequire("../assets/icons/socials/twitter.png");
-const wechat = safeRequire("../assets/icons/socials/wechat.png");
-const whatsapp = safeRequire("../assets/icons/socials/whatsappp.png");
-const yahoo = safeRequire("../assets/icons/socials/yahoo.png");
+const facebook = require("../assets/icons/socials/facebook.png");
+const instagram = require("../assets/icons/socials/instagram.png");
+const linkedin = require("../assets/icons/socials/linkedin.png");
+const messenger = require("../assets/icons/socials/messenger.png");
+const titktok = require("../assets/icons/socials/tiktok.png");
+const twitter = require("../assets/icons/socials/twitter.png");
+const wechat = require("../assets/icons/socials/wechat.png");
+const whatsapp = require("../assets/icons/socials/whatsappp.png");
+const yahoo = require("../assets/icons/socials/yahoo.png");
 
 export default {
     facebook,
