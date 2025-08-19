ffe41e7ce917cc1cb6e3f0dae7f2aa71725cdaa2
Salahaddin50 <salahaddin665@gmail.com>
2025-08-19 17:34:10 +0300

    Fix: Use safeRequire for all image assets in illustrations.ts and images.ts

--- constants/images.ts
+++ constants/images.ts
@@ -1,47 +1,47 @@
-const ornament = require("../assets/images/ornament.png");
-const logo = require("../assets/images/logo.png");
-const logo2 = require("../assets/images/logo2.png");
-const splashOnboarding = require("../assets/images/splash_onboarding.jpg");
+const ornament = safeRequire("../assets/images/ornament.png");
+const logo = safeRequire("../assets/images/logo.png");
+const logo2 = safeRequire("../assets/images/logo2.png");
+const splashOnboarding = safeRequire("../assets/images/splash_onboarding.jpg");
 
-const user1 = require("../assets/images/users/user1.jpeg");
-const user2 = require("../assets/images/users/user2.jpeg");
-const user3 = require("../assets/images/users/user3.jpeg");
-const user4 = require("../assets/images/users/user4.jpeg");
-const user5 = require("../assets/images/users/user5.jpeg");
-const user6 = require("../assets/images/users/user6.jpeg");
-const user7 = require("../assets/images/users/user7.jpeg");
-const user8 = require("../assets/images/users/user8.jpeg");
-const user9 = require("../assets/images/users/user9.jpeg");
-const user10 = require("../assets/images/users/user10.jpeg");
-const user11 = require("../assets/images/users/user11.jpeg");
+const user1 = safeRequire("../assets/images/users/user1.jpeg");
+const user2 = safeRequire("../assets/images/users/user2.jpeg");
+const user3 = safeRequire("../assets/images/users/user3.jpeg");
+const user4 = safeRequire("../assets/images/users/user4.jpeg");
+const user5 = safeRequire("../assets/images/users/user5.jpeg");
+const user6 = safeRequire("../assets/images/users/user6.jpeg");
+const user7 = safeRequire("../assets/images/users/user7.jpeg");
+const user8 = safeRequire("../assets/images/users/user8.jpeg");
+const user9 = safeRequire("../assets/images/users/user9.jpeg");
+const user10 = safeRequire("../assets/images/users/user10.jpeg");
+const user11 = safeRequire("../assets/images/users/user11.jpeg");
 
-const menber1 = require("../assets/images/menbers/user1.jpg");
-const menber2 = require("../assets/images/menbers/user2.jpg");
-const menber3 = require("../assets/images/menbers/user3.jpg");
-const menber4 = require("../assets/images/menbers/user4.jpg");
-const menber5 = require("../assets/images/menbers/user5.jpg");
-const menber6 = require("../assets/images/menbers/user6.jpg");
-const menber7 = require("../assets/images/menbers/user7.jpg");
-const menber8 = require("../assets/images/menbers/user8.jpg");
+const menber1 = safeRequire("../assets/images/menbers/user1.jpg");
+const menber2 = safeRequire("../assets/images/menbers/user2.jpg");
+const menber3 = safeRequire("../assets/images/menbers/user3.jpg");
+const menber4 = safeRequire("../assets/images/menbers/user4.jpg");
+const menber5 = safeRequire("../assets/images/menbers/user5.jpg");
+const menber6 = safeRequire("../assets/images/menbers/user6.jpg");
+const menber7 = safeRequire("../assets/images/menbers/user7.jpg");
+const menber8 = safeRequire("../assets/images/menbers/user8.jpg");
 
-const model1 = require("../assets/images/model1.jpg");
-const model2 = require("../assets/images/model2.jpg");
-const model3 = require("../assets/images/model3.jpg");
-const model4 = require("../assets/images/model4.jpg");
+const model1 = safeRequire("../assets/images/model1.jpg");
+const model2 = safeRequire("../assets/images/model2.jpg");
+const model3 = safeRequire("../assets/images/model3.jpg");
+const model4 = safeRequire("../assets/images/model4.jpg");
 
-const elipseCard = require("../assets/images/elipse-card.png");
-const rectangleCard = require("../assets/images/rectangle-card.png");
+const elipseCard = safeRequire("../assets/images/elipse-card.png");
+const rectangleCard = safeRequire("../assets/images/rectangle-card.png");
 
-const backgroundAvatar1 = require("../assets/images/background_avatar1.jpeg");
-const backgroundAvatar2 = require("../assets/images/background_avatar2.jpeg");
-const backgroundAvatar3 = require("../assets/images/background_avatar3.jpg");
+const backgroundAvatar1 = safeRequire("../assets/images/background_avatar1.jpeg");
+const backgroundAvatar2 = safeRequire("../assets/images/background_avatar2.jpeg");
+const backgroundAvatar3 = safeRequire("../assets/images/background_avatar3.jpg");
 
-const background =  require("../assets/images/background.jpg");
-const avatar = require("../assets/images/avatar.png");
-const face = require("../assets/images/face.png");
-const scanCard = require("../assets/images/scan_card.png");
+const background =  safeRequire("../assets/images/background.jpg");
+const avatar = safeRequire("../assets/images/avatar.png");
+const face = safeRequire("../assets/images/face.png");
+const scanCard = safeRequire("../assets/images/scan_card.png");
 
-const matchCover = require("../assets/images/match_cover.png");
+const matchCover = safeRequire("../assets/images/match_cover.png");
 
 export default {
     ornament,
